50f73b64d5ce2d8f9a45f15c89d0039ea363e13d
Mehmet Selçuk <cezaci80@gmail.com>
2026-03-28 00:03:51 +0300

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,1 +1,17 @@
+from utils.driver_setup import get_driver
 
+def test_login():
+    driver = get_driver()
+    driver.get("https://example.com/login")
+
+    username = driver.find_element("id", "username")
+    password = driver.find_element("id", "password")
+    login_btn = driver.find_element("id", "loginBtn")
+
+    username.send_keys("test_user")
+    password.send_keys("123456")
+    login_btn.click()
+
+    assert "Dashboard" in driver.title
+
+    driver.quit()
